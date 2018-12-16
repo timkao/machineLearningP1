6573b1b9375b41610aedc860922e72f25f60fa28
Tim Kao <pakuya.kao@gmail.com>
2018-12-16 14:31:53 -0500

    histgram created

--- utils/fetchData.py
+++ utils/fetchData.py
@@ -1,5 +1,6 @@
 import os
 import tarfile
+import pandas as pd
 from six.moves import urllib
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
@@ -18,4 +19,6 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
   housing_tgz.extractall(path=housing_path)
   housing_tgz.close()
 
-fetch_housing_data()
+def load_housing_data(housing_path=HOUSING_PATH):
+  csv_path = os.path.join(housing_path, "housing.csv")
+  return pd.read_csv(csv_path)
