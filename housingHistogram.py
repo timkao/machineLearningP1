461c011ac6e3268b4b620011d3c2a072487128be
Tim Kao <pakuya.kao@gmail.com>
2018-12-17 22:02:48 -0500

    correlation coefficient with pandas and sklearn

--- housingHistogram.py
+++ housingHistogram.py
@@ -42,6 +42,49 @@ plt.show()
 
 
 #%%
+import numpy as np
+
+
+#%%
+import os
+import tarfile
+import pandas as pd
+from six.moves import urllib
+from utils.fetchData import load_housing_data
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("dataset", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+housing = load_housing_data(HOUSING_PATH)
+
+
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+housing["income_cat"].hist()
+
+
+#%%
+from sklearn.model_selection import StratifiedShuffleSplit
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_idx, test_idx in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_idx]
+    strat_test_set = housing.loc[test_idx]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+
+#%%
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+
+#%%
 
 
 
